eacde9102fbb1273733416ee6fe10153bc4a399d
Jerry Batorski <jerry.batorski@gmail.com>
2015-01-12 22:48:01 -0600

    Added firebase integration

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -4,123 +4,41 @@
 
 angular.module('tinDev')
   .controller('MainCtrl', function ($scope, $firebase) {
-  	// now we can use $firebase to synchronize data between clients and the server!
-    var ref = new Firebase('https://<your-firebase>.firebaseio.com/');
+    var ref = new Firebase('https://tindev.firebaseio.com/');
     var sync = $firebase(ref);
 
     $scope.currentFramework = {};
-    $scope.frameworks = [
-      {
-        'key': 'angular',
-        'title': 'AngularJS',
-        'url': 'images/logos/angular.png',
-        points:0
-      },
-      {
-        'key': 'bootstrap',
-        'title': 'bootstrap',
-        'url': 'images/logos/bootstrap.png',
-        points:0
-      },
-      {
-        'key': 'canjs',
-        'title': 'canjs',
-        'url': 'images/logos/canjs.png',
-        points:0
-      },
-      {
-        'key': 'dojo',
-        'title': 'dojo',
-        'url': 'images/logos/dojo.png',
-        points:0
-      },
-      {
-        'key': 'ember',
-        'title': 'ember',
-        'url': 'images/logos/ember.png',
-        points:0
-      },
-      {
-        'key': 'firebase',
-        'title': 'firebase',
-        'url': 'images/logos/firebase.png',
-        points:0
-      },
-      {
-        'key': 'foundation',
-        'title': 'foundation',
-        'url': 'images/logos/foundation.png',
-        points:0
-      },
-      {
-        'key': 'grunt',
-        'title': 'grunt',
-        'url': 'images/logos/grunt.png',
-        points:0
-      },
-      {
-        'key': 'ionic',
-        'title': 'ionic',
-        'url': 'images/logos/ionic.png',
-        points:0
-      },
-      {
-        'key': 'jquery',
-        'title': 'jquery',
-        'url': 'images/logos/jquery.png',
-        points:0
-      },
-      {
-        'key': 'knockout',
-        'title': 'knockout',
-        'url': 'images/logos/knockout.png',
-        points:0
-      },
-      {
-        'key': 'react',
-        'title': 'react',
-        'url': 'images/logos/react.png',
-        points:0
-      },
-      {
-        'key': 'yeoman',
-        'title': 'yeoman',
-        'url': 'images/logos/yeoman.png',
-        points:0
-      },
-      {
-        'key': 'vanillajs',
-        'title': 'vanillajs',
-        'url': 'images/logos/vanilla.png',
-        points:0
-      },
-      {
-        'key': 'yui',
-        'title': 'yui',
-        'url': 'images/logos/yui.png',
-        points:0
-      },
-    ];
-    var getRandomFramework = function () {
+    sync.$asArray().$loaded()
+      .then(function (data) {
+        $scope.frameworks = data;
+        getRandomFramework();
+      });
 
+    var getRandomFramework = function () {
       $scope.currentFramework = $scope.frameworks[Math.floor(Math.random() * $scope.frameworks.length)];
     };
 
-    getRandomFramework();
+    $scope.ballin = function () {
+      var item = getItemFromArray();
 
-    $scope.ballin = function(){
-      $scope.frameworks.filter(function(item) {
-        return item.key === $scope.currentFramework.key;
-      })[0].points++;
+      item.points++;
+      $scope.frameworks.$save(item);
 
       getRandomFramework();
     }
 
-    $scope.busted = function(){
-      $scope.frameworks.filter(function(item) {
-        return item.key === $scope.currentFramework.key;
-      })[0].points--;
+    $scope.busted = function () {
+      var item = getItemFromArray();
+
+      item.points--;
+      $scope.frameworks.$save(item);
 
       getRandomFramework();
+    }
+
+    var getItemFromArray = function(){
+     return  $scope.frameworks.filter(function (item) {
+        return item.key === $scope.currentFramework.key;
+      })[0];
     }
   });
